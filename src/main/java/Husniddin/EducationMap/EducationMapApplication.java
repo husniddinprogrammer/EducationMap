66420ee249dee6a0a2132b079887528792b2782f
husniddinprogrammer <husniddinprogrammer@gmail.com>
2021-10-11 10:29:41 +0500

    Education Map start

---
+++ src/main/java/Husniddin/EducationMap/EducationMapApplication.java
@@ -0,0 +1,13 @@
+package Husniddin.EducationMap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducationMapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Husniddin/EducationMap/EducationMapApplicationTests.java
@@ -0,0 +1,13 @@
+package Husniddin.EducationMap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
